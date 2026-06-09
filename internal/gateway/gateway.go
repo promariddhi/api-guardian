d85082a93ece53a881799bcffb0ccea7120815d4
promariddhi <114817746+promariddhi@users.noreply.github.com>
2026-06-09 17:39:41 +0530

    fix: implement longest path matching for routing

--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -11,11 +11,17 @@ type Gateway struct {
 }
 
 func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var matched *proxy.ReverseProxy
+	longest := 0
 	for path, proxy := range g.Proxies {
-		if strings.HasPrefix(r.URL.Path, path) {
-			proxy.Handler.ServeHTTP(w, r)
-			return
+		if strings.HasPrefix(r.URL.Path, path) && len(path) > longest {
+			matched = proxy
+			longest = len(path)
 		}
+	}
+	if matched != nil {
+		matched.Handler.ServeHTTP(w, r)
+		return
 	}
 	http.NotFound(w, r)
 }
