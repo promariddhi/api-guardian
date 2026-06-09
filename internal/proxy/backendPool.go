3d7ed66e094f0f62671c39d074d3711ff92c9282
promariddhi <114817746+promariddhi@users.noreply.github.com>
2026-06-09 18:00:50 +0530

    feat: add backend failure detection, retries for GET method and failure recovery.

--- internal/proxy/backendPool.go
+++ internal/proxy/backendPool.go
@@ -2,35 +2,96 @@ package proxy
 
 import (
 	"log"
+	"net/http"
 	"net/url"
 	"sync"
+	"time"
 )
 
-type BackendPool struct {
-	backends []*url.URL
+type backendPool struct {
+	backends []backend
 	current  int
 	mu       sync.Mutex
 }
 
-func NewBackendPool(urls []string) *BackendPool {
-	backends := make([]*url.URL, len(urls))
+type backend struct {
+	url          *url.URL
+	alive        bool
+	failureCount int
+}
+
+func newBackendPool(urls []string) *backendPool {
+	backends := make([]backend, len(urls))
 	for i, urlString := range urls {
 		if url, err := url.Parse(urlString); err == nil {
-			backends[i] = url
+			backends[i] = backend{url: url, alive: true}
 		} else {
 			log.Fatal("error when making backend pool")
 		}
 	}
-	return &BackendPool{
+	return &backendPool{
 		backends: backends,
 		current:  0,
 	}
 }
 
-func (p *BackendPool) Next() *url.URL {
+func (p *backendPool) next() *backend {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	p.current = (p.current + 1) % len(p.backends)
-	return p.backends[p.current]
+	for i := 1; i <= len(p.backends); i++ {
+		current := (p.current + i) % len(p.backends)
+		if p.backends[current].alive {
+			p.current = current
+			return &p.backends[p.current]
+		}
+	}
+	return nil
+}
+
+func (b *backend) markFailure() {
+	b.failureCount++
+	if b.failureCount > 3 {
+		b.alive = false
+	}
+}
+
+func (p *backendPool) startHealthChecks() {
+	ticker := time.NewTicker(5 * time.Second)
+
+	go func() {
+		defer ticker.Stop()
+		for range ticker.C {
+			p.checkDeadBackends()
+		}
+	}()
+}
+
+func (p *backendPool) checkDeadBackends() {
+	p.mu.Lock()
+	var dead []*backend
+	for i := range p.backends {
+		if !p.backends[i].alive {
+			dead = append(dead, &p.backends[i])
+
+		}
+	}
+	p.mu.Unlock()
+	for _, backend := range dead {
+		backend.probeBackend()
+	}
+}
+
+func (b *backend) probeBackend() {
+	client := http.Client{
+		Timeout: 2 * time.Second,
+	}
+	resp, err := client.Get(b.url.String())
+	if err != nil {
+		return
+	}
+	resp.Body.Close()
+	b.alive = true
+	b.failureCount = 0
+	log.Printf("backend recovered: %s", b.url.String())
 }
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -17,29 +17,38 @@ type ReverseProxy struct {
 	Path         string
 	Handler      http.Handler
 	AllowedRoles []string
-	BackendPool  *BackendPool
+	BackendPool  *backendPool
 }
 
 func NewReverseProxy(path string, route config.Route, rdb *redis.Client, ctx context.Context) *ReverseProxy {
-	pool := NewBackendPool(route.Backends)
-	proxy := &httputil.ReverseProxy{
-		Rewrite: func(pr *httputil.ProxyRequest) {
-			target := pool.Next()
-			log.Println("Forwarding to:", target.String())
+	pool := newBackendPool(route.Backends)
+	pool.startHealthChecks()
 
-			pr.SetURL(target)
-			if route.TrimPrefix {
-				pr.Out.URL.Path = strings.TrimPrefix(pr.In.URL.Path, path)
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		backend := pool.next()
+		if backend == nil {
+			http.Error(w, "service unavailable", http.StatusServiceUnavailable)
+			return
+		}
+		if err := forward(w, r, path, route.TrimPrefix, backend); err != nil {
+			backend.markFailure()
+			if r.Method == "GET" {
+				backend = pool.next()
+				if backend == nil {
+					http.Error(w, "service unavailable", http.StatusServiceUnavailable)
+					return
+				}
+				if err = forward(w, r, path, route.TrimPrefix, backend); err != nil {
+					backend.markFailure()
+					http.Error(w, "service unavailable", http.StatusServiceUnavailable)
+					return
+				}
 			}
-			pr.SetXForwarded()
-		},
-	}
+			http.Error(w, "service unavailable", http.StatusServiceUnavailable)
+		}
 
-	proxy.Transport = &http.Transport{
-		ResponseHeaderTimeout: 3 * time.Second,
-	}
+	})
 
-	handler := http.Handler(proxy)
 	if route.Protected {
 		handler = middleware.Auth(middleware.UserRateLimiter(rdb, ctx, handler), route.AllowedRoles)
 	}
@@ -50,4 +59,31 @@ func NewReverseProxy(path string, route config.Route, rdb *redis.Client, ctx con
 		AllowedRoles: route.AllowedRoles,
 		BackendPool:  pool,
 	}
+}
+
+func forward(w http.ResponseWriter, r *http.Request, path string, trim bool, backend *backend) error {
+	target := backend.url
+	proxy := &httputil.ReverseProxy{}
+
+	proxy.Rewrite = func(pr *httputil.ProxyRequest) {
+
+		log.Printf("Forwarding to: %s", target.String())
+
+		pr.SetURL(target)
+		if trim {
+			pr.Out.URL.Path = strings.TrimPrefix(pr.In.URL.Path, path)
+		}
+		pr.SetXForwarded()
+	}
+	proxy.Transport = &http.Transport{
+		ResponseHeaderTimeout: 3 * time.Second,
+	}
+
+	var proxyErr error
+	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+		proxyErr = err
+	}
+
+	proxy.ServeHTTP(w, r)
+	return proxyErr
 }
